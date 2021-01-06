db1a46b4a0874c52b34a344a29bf22b8e83921e8
CalvinY8 <46547265+CalvinY8@users.noreply.github.com>
2021-01-05 18:31:02 -0800

    reformatted code

--- crawler.py
+++ crawler.py
@@ -10,12 +10,8 @@ from selenium import webdriver
 import time
 from datetime import timedelta
 
-timeStamp_list = []
-img_url_list = []
-driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\chromedriver.exe")
-
-def getBongoPlayerFromURL():
-    url = "https://bongo-ca.youseeu.com/spa/external-player/107762/cd7106029923b1e9a9ede8f229413946/styled?lti-scope=d2l-resource-syncmeeting-list"
+#use the driver to remove the playerDiv from the html from the url.
+def getPlayerDivFromURL(driver, url):
     driver.get(url)
     time.sleep(15) #wait for page to load
 
@@ -28,47 +24,37 @@ def getBongoPlayerFromURL():
     content = container.find_element_by_class_name("content")
     paper = content.find_element_by_class_name("md-paper")
     playerDiv = paper.find_element_by_class_name("c-bbb-player")
-    
-    #from playerTop, extract 1) the total video length, and 2) extract the mp4
-    playerTop = playerDiv.find_element_by_class_name("c-bbb-player__top")
-    player = playerTop.find_element_by_class_name("c-bbb-player__player")
-    inner_player = player.find_element_by_class_name("c-player")
-    #1) extract total vid length
-    #find video length
 
+    return playerDiv
+
+#takes a section of webElement, called "inner_player"
+#returns total video length in HH:MM:SS format
+def getTotalVidLength(inner_player):
     controls = inner_player.find_element_by_class_name("c-player__controls")
     currenttime = controls.find_element_by_class_name("c-player__current-time")
     duration = currenttime.find_element_by_class_name("c-player__duration")
     total_vid_length = duration.get_attribute('innerHTML')
     total_vid_length = str("00:" + total_vid_length)
-
-    #2) extract mp4
-    vidWrapper = inner_player.find_element_by_class_name("c-player__video-wrapper")
-    vid = vidWrapper.find_element_by_class_name("vjs-tech")
-    mp4_url = vid.get_attribute("src")
-
-    #from player bottom, extract slides
-    playerBottom = playerDiv.find_element_by_class_name("c-bbb-player__bottom")
-    thumbnailsContainer = playerBottom.find_element_by_class_name("c-bbb-player__thumbnails")
-    extractSlides(thumbnailsContainer)
-
-    driver.quit()
-    mp4_to_mp3(mp4_url) #LINE TO ENABLE MP3 DOWNLOAD
     return total_vid_length
 
-#convert mp4 to mp3
+#takes mp4 url
+#downloads the audio only as "audio.mp3"
 def mp4_to_mp3(url):
     cmd = "ffmpeg -i " + url + " -vn audio.mp3"
     subprocess.call(cmd,shell=True)
-
 
 
 def deleteFileNameIfExists(fileName):
     if os.path.exists(fileName):
         os.remove(fileName)
 
-#extract the slides
-def extractSlides(thumbnailsContainer):
+#identify the slides in the slideshow, given playerDiv
+#for each slide, append img_url and timestamp to their respective lists.
+def get_slides_data(playerDiv, driver):
+    timeStamp_list = []
+    img_url_list = []
+    playerBottom = playerDiv.find_element_by_class_name("c-bbb-player__bottom")
+    thumbnailsContainer = playerBottom.find_element_by_class_name("c-bbb-player__thumbnails")
 
     for thumb in thumbnailsContainer.find_elements_by_class_name("c-bbb-player__thumbnail"):
         #---IMG 
@@ -79,6 +65,8 @@ def extractSlides(thumbnailsContainer):
         driver.execute_script("arguments[0].style.visibility = 'visible'", time_Ptag) #set visible so selenium can see element
         timeStamp_list.append(time_Ptag.text)
 
+    return (timeStamp_list, img_url_list)
+
 
 #takes list of timestamps of format "HH:MM:SS"
 #returns list of durations in seconds
@@ -87,7 +75,10 @@ def hms_to_seconds(t):
     secs = int(timedelta(hours=int(h),minutes=int(m),seconds=int(s)).total_seconds())
     return secs
 
-def timestamps_to_durations(total_vid_length):
+#takes list of timestamps of format "HH:MM:SS"
+#appends total_vid_length to list
+#calculates list of durations for each image to appear in lecture.mp4
+def timestamps_to_durations(timeStamp_list):
     durations = []
     frmat = '%H:%M:%S' #input format
     timeStamp_list.append(total_vid_length)
@@ -100,9 +91,16 @@ def timestamps_to_durations(total_vid_length):
     return durations
 
 
-#using timeStamp_list and img_url_list
-def create_demuxer_txt(total_vid_length):
-    durations_list = timestamps_to_durations(total_vid_length)
+#using timeStamp_list and img_url_list, create txt file for ffmpeg to make into mp4 w/o audio
+def create_demuxer_txt(total_vid_length, slidesData_tuple):
+    #unpack tuple into slides data lists
+    timeStamp_list, img_url_list = slidesData_tuple
+
+    #the last slide requires both timestamp slide appears, and the time the video ends to calculate duration
+    timeStamp_list.append(total_vid_length)
+
+    #durations for how long each slide appears
+    durations_list = timestamps_to_durations(timeStamp_list)
 
     f = open("demuxerfile.txt", "w")
 
@@ -123,7 +121,10 @@ def create_demuxer_txt(total_vid_length):
     durations_list.clear()
     img_url_list.clear()
 
-    #slides only
+
+#create the final lecture.mp4 of both audio and slides
+def create_lecture_mp4():
+    #creates an mp4 of slides only, called "slides.mp4"
     command = "ffmpeg -f concat -safe 0 -protocol_whitelist file,http,https,tcp,tls -i demuxerfile.txt -vsync vfr -pix_fmt yuv420p slides.mp4"
     subprocess.call(command,shell=True)
 
@@ -131,20 +132,56 @@ def create_demuxer_txt(total_vid_length):
     cmd = "ffmpeg -i slides.mp4 -i audio.mp3 -c copy -map 0:v:0 -map 1:a:0 lecture.mp4"
     subprocess.call(cmd,shell=True)
 
+#clear all files other than lecture.mp4 and this file(crawler.py)
 def clearLeftoverFiles():
     deleteFileNameIfExists("audio.mp3")
     deleteFileNameIfExists("demuxerfile.txt")
     deleteFileNameIfExists("slides.mp4")
 
+
+def get_mp4_url(inner_player):
+    #extract mp4 url
+    vidWrapper = inner_player.find_element_by_class_name("c-player__video-wrapper")
+    vid = vidWrapper.find_element_by_class_name("vjs-tech")
+    mp4_url = vid.get_attribute("src")
+    return mp4_url
+
+def get_inner_player_from_playerDiv(playerDiv):
+    playerTop = playerDiv.find_element_by_class_name("c-bbb-player__top")
+    player = playerTop.find_element_by_class_name("c-bbb-player__player")
+    inner_player = player.find_element_by_class_name("c-player")
+    return inner_player
+
 if __name__ == '__main__':
     start_time = time.time()
-
     clearLeftoverFiles()
-
     deleteFileNameIfExists("lecture.mp4")
-    total_vid_length = getBongoPlayerFromURL()
-    create_demuxer_txt(total_vid_length) #create new mp4
+
+
+
+    driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\chromedriver.exe")
+    url = "https://bongo-ca.youseeu.com/spa/external-player/107762/cd7106029923b1e9a9ede8f229413946/styled?lti-scope=d2l-resource-syncmeeting-list"
+    playerDiv = getPlayerDivFromURL(driver, url) #retrieve the player div
+
+    #---from the playerdiv, put the slide data into the timeStamp_list and img_url_list, which are lists held in a tuple
+    slidesData_tuple = get_slides_data(playerDiv, driver)
+
+    #get the inner_player, because it holds the total_vid_length and the mp4_url...
+    inner_player = get_inner_player_from_playerDiv(playerDiv)
+
+    #---from the innerPlayer, get the total vid length and the mp4 url
+    total_vid_length = getTotalVidLength(inner_player)
+    mp4_url = get_mp4_url(inner_player)
+
+    #done using webdriver
+    driver.quit()
+
+    #download the mp4_url to file called "audio.mp3"
+    mp4_to_mp3(mp4_url)
+    create_demuxer_txt(total_vid_length, slidesData_tuple) #create demuxer txt for ffmpeg to use
+    create_lecture_mp4()
+
+
 
     clearLeftoverFiles()
-
     print("--- %s seconds ---" % (time.time() - start_time))
