ea269fc7cfd782521ea6f00353073d4b4980709c
CalvinY8 <46547265+CalvinY8@users.noreply.github.com>
2021-01-05 16:08:48 -0800

    made more efficient

--- crawler.py
+++ crawler.py
@@ -12,7 +12,6 @@ import subprocess #needed to run ffmpeg commands
 import glob
 import urllib.request
 from selenium import webdriver
-from bs4 import BeautifulSoup
 from moviepy.editor import *
 import time
 from datetime import timedelta
@@ -22,7 +21,6 @@ img_url_list = []
 driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\chromedriver.exe")
 
 def getBongoPlayerFromURL():
-
     url = "https://bongo-ca.youseeu.com/spa/external-player/107762/cd7106029923b1e9a9ede8f229413946/styled?lti-scope=d2l-resource-syncmeeting-list"
     driver.get(url)
     time.sleep(15) #wait for page to load
@@ -54,7 +52,6 @@ def getBongoPlayerFromURL():
     vidWrapper = inner_player.find_element_by_class_name("c-player__video-wrapper")
     vid = vidWrapper.find_element_by_class_name("vjs-tech")
     mp4_url = vid.get_attribute("src")
-    mp4_to_mp3(mp4_url) #LINE TO ENABLE MP3 DOWNLOAD
 
     #from player bottom, extract slides
     playerBottom = playerDiv.find_element_by_class_name("c-bbb-player__bottom")
@@ -62,24 +59,14 @@ def getBongoPlayerFromURL():
     extractSlides(thumbnailsContainer)
 
     driver.quit()
+    mp4_to_mp3(mp4_url) #LINE TO ENABLE MP3 DOWNLOAD
     return total_vid_length
-
-def downloadImages():
-    counter = 0
-    for imageURL in img_url_list:
-        urllib.request.urlretrieve(imageURL, "images/img" + str(counter) + ".png")
-        counter += 1
-
-def emptyImagesFolder():
-    files = glob.glob('images/*')
-    for f in files:
-        os.remove(f)
 
 #convert to mp3
 def mp4_to_mp3(url):
     #use the url, download as input.mp4
     urllib.request.urlretrieve(url, "input.mp4")
-    time.sleep(5)
+    # time.sleep(5)
 
     #do the conversion
     videoClip = VideoFileClip("input.mp4")
@@ -97,17 +84,16 @@ def deleteFileNameIfExists(fileName):
 
 #extract the slides
 def extractSlides(thumbnailsContainer):
+
     for thumb in thumbnailsContainer.find_elements_by_class_name("c-bbb-player__thumbnail"):
         #---IMG
         imgtag = thumb.find_element_by_class_name("c-bbb-player__thumbnail-image")
-        imgsrc = imgtag.get_attribute("src")
-        img_url_list.append(imgsrc)
+        img_url_list.append(imgtag.get_attribute("src"))
 
         #---TIMESTAMP
         time_Ptag = thumb.find_element_by_class_name("c-bbb-player__thumbnail-time")
-        driver.execute_script("arguments[0].style.visibility = 'visible'", time_Ptag)
-        timestamp = time_Ptag.text
-        timeStamp_list.append(timestamp)
+        driver.execute_script("arguments[0].style.visibility = 'visible'", time_Ptag) #set visible so selenium can see element
+        timeStamp_list.append(time_Ptag.text)
 
 
 #takes list of timestamps of format "HH:MM:SS"
@@ -122,12 +108,11 @@ def timestamps_to_durations(total_vid_length):
     frmat = '%H:%M:%S' #input format
     timeStamp_list.append(total_vid_length)
     #add the total vid length as the last timestamp. this is to calculate duration for the last image requires both the timestamp the image appears, and timestamp image is ended
-    for count, item in enumerate(timeStamp_list):
-        if count > 0:
-            final  = hms_to_seconds(timeStamp_list[count])
-            initial = hms_to_seconds(timeStamp_list[count-1])
-            #get the difference in seconds
-            durations.append(int((final - initial)))
+
+    for i in range(1, len(timeStamp_list)):
+        final  = hms_to_seconds(timeStamp_list[i])
+        initial = hms_to_seconds(timeStamp_list[i-1])
+        durations.append(int((final - initial)))         #get the difference in seconds
     return durations
 
 
@@ -136,24 +121,26 @@ def create_demuxer_txt(total_vid_length):
     durations_list = timestamps_to_durations(total_vid_length)
 
     f = open("demuxerfile.txt", "w")
-    images_list=os.listdir("images")
 
     #due to a quirk in ffmpeg, the last image file path needs to be written twice in the txt,
     #store the last image file path in this variable
-    last_img_filepath = images_list[-1]
+    last_img_url = img_url_list[-1]
 
-    for i, (duration, img_relative_path) in enumerate(zip(durations_list, images_list)):
+    for i, (duration, img_url) in enumerate(zip(durations_list, img_url_list)):
         #this is the order to write to the text file...each f.write is one line.
-        f.write("file " + "'images/" + img_relative_path + "'" + "\n")
+        f.write("file " + "'" + img_url + "'" + "\n")
         f.write("duration " + str(duration) + "\n")
-        if i == (len(images_list) - 1): #you have reached the last index. b/c .py indicing starts at zero, the last index is one less than the element count.
-            f.write("file " + "'images/" + last_img_filepath + "'")
+        if i == (len(img_url_list) - 1): #you have reached the last index. b/c .py indicing starts at zero, the last index is one less than the element count.
+            f.write("file " + "'" + last_img_url + "'")
 
     
     f.close()
 
+    durations_list.clear()
+    img_url_list.clear()
+
     #slides only
-    command = "ffmpeg -f concat -i demuxerfile.txt -vsync vfr -pix_fmt yuv420p slides.mp4"
+    command = "ffmpeg -f concat -safe 0 -protocol_whitelist file,http,https,tcp,tls -i demuxerfile.txt -vsync vfr -pix_fmt yuv420p slides.mp4"
     subprocess.call(command,shell=True)
 
     #adding audio
@@ -161,18 +148,19 @@ def create_demuxer_txt(total_vid_length):
     subprocess.call(cmd,shell=True)
 
 def clearLeftoverFiles():
-    emptyImagesFolder() #delete any files remaining from previous runs of code.
     deleteFileNameIfExists("audio.mp3")
     deleteFileNameIfExists("demuxerfile.txt")
     deleteFileNameIfExists("slides.mp4")
 
 if __name__ == '__main__':
+    start_time = time.time()
+
     clearLeftoverFiles()
 
     deleteFileNameIfExists("lecture.mp4")
     total_vid_length = getBongoPlayerFromURL()
-    downloadImages() #LINE TO ENABLE IMAGE DOWNLOADING
     create_demuxer_txt(total_vid_length) #create new mp4
 
     clearLeftoverFiles()
 
+    print("--- %s seconds ---" % (time.time() - start_time))
